0f9f6e7f00ed39b2da956961c2e6219f791f00ec
Rafael Ferreira <rafaelferreira@iMac-de-Rafael.local>
2020-12-16 00:04:57 -0300

    sucesso na conexão com Firebase

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {ScrollView, Text} from 'react-native';
 import BleManager from 'react-native-ble-manager';
+import database from '@react-native-firebase/database';
 
+const reference = database().ref('/users/123');
 const id = '24:62:ab:dc:d8:fe';
+
 export default function App() {
   function conectar() {
     BleManager.connect(id)
@@ -27,18 +30,28 @@ export default function App() {
   }
   function scan() {
     console.log('foi');
-    BleManager.getDiscoveredPeripherals([]).then((peripheralsArray) => {
+    BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {
       for (let i of peripheralsArray) {
         console.log('=====================================');
         console.log(i);
       }
     });
   }
+
+  function readDatabase() {
+    database()
+      .ref('/')
+      .once('value')
+      .then(snapshot => {
+        console.log('User data: ', snapshot.val());
+      });
+  }
   return (
     <ScrollView>
       <Text onPress={() => conectar()}>Ligar O bluetooth</Text>
       <Text onPress={() => disconectar()}>deslifar O bluetooth</Text>
       <Text onPress={() => scan()}>SCANs</Text>
+      <Text onPress={() => readDatabase()}>Banco</Text>
     </ScrollView>
   );
 }
